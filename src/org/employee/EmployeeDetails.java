879de4d18acdfc486068a30c3de4cf5a43e1c3e8
anangitacc <anand.akmail@gmail.com>
2022-07-01 18:45:11 +0530

    firstinfo

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package org.employee;
+
+public class EmployeeDetails {
+
+	// method 1
+	private void empName() {
+
+		System.out.println("Employee name : Anand");
+	}
+
+	// method 2
+	private void empMobile() {
+
+		System.out.println("Employee mobile : 8939278836");
+	}
+
+	// main method
+	public static void main(String[] args) {
+
+		// obj
+		EmployeeDetails e = new EmployeeDetails();
+
+		// method call
+		e.empName();
+		e.empMobile();
+
+	}
+
+}
